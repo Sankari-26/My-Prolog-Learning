7ce2d4776eba53c28032502620317e3aece2c03c
Sankari-26 <sankari.ug.23.cb@francisxavier.ac.in>
2026-09-02 08:00:21 +0000

    Family Tree

---
+++ exp2.pl
@@ -0,0 +1,23 @@
+% Parent facts
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+parent(liz, jim).
+
+% Who is male, who is female
+male(tom).
+male(bob).
+male(pat).
+male(jim).
+female(pam).
+female(liz).
+female(ann).
+
+% Rules
+sister(X, Y) :- female(X), parent(Z, X), parent(Z, Y).
+brother(X, Y) :- male(X), parent(Z, X), parent(Z, Y).
+grandma(X, Y) :- female(X), parent(X, Z), parent(Z, Y).
+grandpa(X, Y) :- male(X), parent(X, Z), parent(Z, Y).
